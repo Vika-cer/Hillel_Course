66096e82489342c07b455660445207f57c222750
Vika-cer <139149303+Vika-cer@users.noreply.github.com>
2023-09-25 13:08:32 +0300

    Create SubMenu.cs

---
+++ Less7/Hw2/SubMenu.cs
@@ -0,0 +1,12 @@
+namespace project
+{
+    enum SubMenu
+    {
+        Mathematics = 1,
+        Ukrainian = 2,
+        English = 3,
+        History = 4,
+        Exit = 5,
+        None,
+    }
+}
